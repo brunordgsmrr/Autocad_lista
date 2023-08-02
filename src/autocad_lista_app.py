80f69b1c24f6c515628845d85f696fccbc41dc40
Bruno Moreira <bruno_rdgs_@hotmail.com>
2023-08-02 16:16:34 -0300

    Added feature: function to search an config file

--- src/autocad_lista_app.py
+++ src/autocad_lista_app.py
@@ -1,13 +1,47 @@
+import os
+import shutil
+import tkinter
 import autocad_lista_class as lista
 import pyperclip
+from tkinter import filedialog as fd
 
 LISTA = []
 OUTPUT_OBJ = []
 OUTPUT_TEXT = ''
+USER = os.getenv('USERPROFILE')
+PCP_PATH = f'{USER}/Documents/pcp_cad'
 
 try:
-    with open('./config.txt', 'r', encoding='utf-8') as file:
-        itens_ignorados = file.readlines()
+    try:
+        with open(f'{PCP_PATH}/config.txt', 'r', encoding='utf-8') as file:
+            itens_ignorados = file.readlines()
+    except Exception as err:
+        print(f'Falha no script\n{err}')
+        print('Pressione qualquer tecla para procurar o arquivo "config.txt"')
+        input()
+        filetypes = (('Text', '*.txt'), ('All files', '*.*'))
+
+        root = tkinter.Tk()
+        config_path = fd.askopenfilename(title='Open config file', initialdir=f'{USER}/Documents', filetypes=filetypes)
+        root.destroy()
+        root.mainloop()
+
+        file_name = os.path.basename(config_path)
+        try:
+            os.makedirs(PCP_PATH)
+        except FileExistsError:
+            print('\nPasta já existe\n')
+
+        shutil.copyfile(config_path, f'{PCP_PATH}/{file_name}')
+
+        print(f'Arquivo selecionado: {config_path}')
+        print('Pressione qualquer tecla para prosseguir...')
+        input()
+    finally:
+        with open(f'{PCP_PATH}/config.txt', 'r', encoding='utf-8') as file:
+            itens_ignorados = file.readlines()
+
+
     #with open('utils/lista_de_exemplo.txt', 'r', encoding='utf-8') as file:
     #    conteudo = file.readlines()
     #    for i in conteudo:
@@ -28,6 +62,6 @@ try:
         OUTPUT_TEXT = f'{OUTPUT_TEXT}{i.get_lista()}\n'
     pyperclip.copy(OUTPUT_TEXT)
 
-except ValueError:
-    print("Falha no script")
+except Exception as err:
+    print(f'Falha no script\n{err}')
     input()
